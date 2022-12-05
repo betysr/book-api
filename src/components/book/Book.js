9cf003f57192d09744789caff5fff7f987e68033
betysr <betysr96@gmail.com>
2022-12-05 14:52:19 +0100

    useNavigate hooks used for detail page

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -2,11 +2,13 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import "./books.css";
 import FavIcon from "../book/FavIcon";
+import { useNavigate } from "react-router-dom";
 
 function Book({ book }) {
+  const navigate = useNavigate();
   return (
     <Container>
-      <div className="card cover" style={{ width: "100%", height: 400 }}>
+      <div className="card" style={{ width: "100%", height: 400 }} onClick={()=>navigate(`/book/${book.id}`)}>
       <a className="addIcon"><FavIcon book={book}></FavIcon></a>
         <img src={book.image_url} alt="Avatar" height={300} />
         <div className="card-body">
